6e7d037f537917e8b718e30093f80a863107dd6c
Thomas Dick <thomas.ed.dick@gmail.com>
2022-11-14 13:32:15 -0700

    add lists

--- app/src/main/java/com/example/wallpaperjetpack/MainActivity.kt
+++ app/src/main/java/com/example/wallpaperjetpack/MainActivity.kt
@@ -3,24 +3,30 @@ package com.example.wallpaperjetpack
 import android.app.WallpaperManager
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.wallpaperjetpack.ui.theme.WallpaperjetpackTheme
+import java.io.IOException
 
 
 class MainActivity : ComponentActivity() {
@@ -42,22 +48,62 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun StartScreen(name: String) {
-    Text(text = "Hello $name!")
-    Image(
-        painterResource(id = R.drawable.ic_opp),
-        contentDescription = stringResource(id = R.string.testing),
-        modifier = Modifier.fillMaxSize()
-    )
-    SimpleButton()
+    val context = LocalContext.current
+    val collections = listOf("Hello", "this", "is", "a", "test")
+
+    LazyColumn(){
+        for (collection in collections){
+            card(text = collection, resid = R.drawable.ic_opp)
+        }
+    }
+
+    //SimpleButton {setWallpaper(R.drawable.ic_opp, context)}
+
+}
+
+
+fun setWallpaper(resid: Int, context: Context){
+    val wallpaperManager = WallpaperManager.getInstance(context)
+    try {
+        wallpaperManager.setResource(resid)
+    } catch (e: IOException) {
+        e.printStackTrace()
+    }
 }
 
 @Composable
-fun SimpleButton() {
+fun SimpleButton(func: () -> Unit) {
     val context = LocalContext.current
     Button(onClick = {
-        Toast.makeText(context, "Button clicked", Toast.LENGTH_SHORT)
-    }, modifier = Modifier.size(100.dp)) {
+        func()
+        Toast.makeText(context, "Wallpaper Set Successfully", Toast.LENGTH_SHORT).show()
+    }, modifier = Modifier.size(50.dp, 50.dp)) {
         Text(text = "Simple Button")
+    }
+}
+
+@Composable
+fun card(text: String, resid: Int){
+    Card(shape = RoundedCornerShape(10.dp), modifier = Modifier
+        .fillMaxWidth()
+        .padding(20.dp)) {
+        Row() {
+            Text(
+                text = text,
+                Modifier
+                    .fillMaxWidth(0.6f)
+                    .padding(vertical = 45.dp),
+                textAlign = TextAlign.Center,
+                fontSize = 30.sp
+            )
+            Image(
+                painter = painterResource(id = resid),
+                contentDescription = "Collection Picture",
+                modifier = Modifier
+                    .padding(5.dp)
+                    .clip(RoundedCornerShape(10.dp))
+            )
+        }
     }
 }
 
@@ -65,6 +111,6 @@ fun SimpleButton() {
 @Composable
 fun DefaultPreview() {
     WallpaperjetpackTheme {
-        StartScreen("Android")
+        card("hello", R.drawable.ic_opp)
     }
 }
